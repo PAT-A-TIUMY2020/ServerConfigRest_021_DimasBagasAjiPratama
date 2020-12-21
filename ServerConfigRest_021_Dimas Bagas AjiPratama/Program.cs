d6e745ab06ebaae03ee59f8c8c54213482e4a423
DimasBagasAjipratama <56101230+DimasBagasAjipratama@users.noreply.github.com>
2020-12-21 11:47:21 +0700

    Add project files.

---
+++ ServerConfigRest_021_Dimas Bagas AjiPratama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_021_Dimas_Bagas_AjiPratama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
